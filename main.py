bd5db7f14b04b3c7cd7b83d21ab1b7e5c0c448d4
Shu-Ann <a58214771@gmail.com>
2023-10-03 02:33:41 +1100

    main

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 '''
 train and save the model pipeline
 '''
-# clean data -> combine data (text)
-# split audio file -> generate MFCC (audio)
+from setting import config
+from utils.audio import create_audio_loader
+from utils.berthelper import create_text_loader
+from utils.multi import create_multi_loader
+from utils import train
+from model import *
+
+
+
+if config.mode == "text":
+    train_dataloader = create_text_loader(ds, shuffle=True)
+    eval_dataloader = create_text_loader(ds, shuffle=True)
+    test_dataloader = create_text_loader(ds, shuffle=False)
+
+elif config.mode == "audio":
+    train_dataloader = create_audio_loader(ds, shuffle=True)
+    eval_dataloader = create_audio_loader(ds, shuffle=True)
+    test_dataloader = create_audio_loader(ds, shuffle=False)
+
+elif config.mode == "multi":
+    train_dataloader = create_multi_loader(ds, shuffle=True)
+    eval_dataloader = create_multi_loader(ds, shuffle=True)
+    test_dataloader = create_multi_loader(ds, shuffle=False)
+
+ 
 # ------   text 5/10 shots & 80-20  -------
-# train model -> save model -> predict
+# train model -> save model
 
 # ------ audio resnet50/ resnet152---------
-# train model -> save model -> predict
+# train model -> save model 
 
 # ---------- multimodal ------------
-# concat text embedding & audio embedding -> train model -> save model -> predict
+# concat text embedding & audio embedding -> train model -> save model 
 
 
 
